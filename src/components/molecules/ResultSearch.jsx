d034827fe0dbe2a6f46c20c9e377a2d9ff1d66c9
TamaraElyyan <tamaraelyyan1@gmail.com>
2024-08-28 00:03:20 +0300

    Update ResultSearch.jsx

--- src/components/molecules/ResultSearch.jsx
+++ src/components/molecules/ResultSearch.jsx
@@ -5,7 +5,7 @@ import HalfStar from '../../asserts/PNG/halfstar.png';
 
 const ResultSearch = () => {
     return (
-      <div className="flex bg-white shadow-md rounded-md overflow-hidden w-full max-w-4xl border border-gray-200 mt-[304px]">
+      <div className="flex bg-white p-4 shadow-md rounded-md overflow-hidden w-full max-w-4xl border border-gray-200 mt-[304px]">
         {/* Left: Hotel Image */}
         <img
           src={HotelImage}
