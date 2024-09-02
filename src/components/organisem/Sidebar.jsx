fbd3da2d2efeb653a840b68515dcb2f1589065fe
TamaraElyyan <tamaraelyyan1@gmail.com>
2024-09-02 17:28:42 +0300

    Updated SideBar

--- src/components/organisem/Sidebar.jsx
+++ src/components/organisem/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Search from '../molecules/Search';
 import Price from '../molecules/Price';
 import Rating from '../molecules/Rating';
@@ -6,16 +6,55 @@ import Category from '../molecules/Category';
 import Activities from '../molecules/Activities';
 
 export default function Sidebar() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  // Prevent body scroll when sidebar is open
+  useEffect(() => {
+    if (isSidebarOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [isSidebarOpen]);
+
   return (
-    <div className='container flex flex-col pl-4 sm:pl-8 md:pl-16 space-y-4 md:space-y-6 mt-12 sm:mt-16 md:mt-24 cursor-auto'>
-      <Search />
-      <div className='ml-4'>
-        <h1 className='text-gray-800 text-lg font-bold'>Filter by</h1>
+    <div className="relative">
+      {/* Button to toggle the sidebar on mobile */}
+      <button
+        className="sm:hidden px-4 py-2 text-sm font-medium text-gray-700 bg-gray-100 rounded-md"
+        onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+      >
+        Filters +
+      </button>
+
+      {/* Overlay for mobile */}
+      {isSidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black opacity-50 z-30 sm:hidden"
+          onClick={() => setIsSidebarOpen(false)}
+        />
+      )}
+
+      {/* Sidebar */}
+      <div
+        className={`fixed inset-y-0 left-5 transform bg-white w-64 transition-transform duration-300  z-40 overflow-y-auto ${
+          isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+        } sm:relative sm:translate-x-0 sm:w-72`}
+      >
+        <div className="container flex flex-col space-y-4 sm:space-y-6 mt-12 sm:mt-16 md:mt-24 cursor-auto">
+          <Search />
+          <div className="ml-4">
+            <h1 className="text-gray-800 text-lg font-bold">Filter by</h1>
+          </div>
+          <Price />
+          <Rating />
+          <Category />
+          <Activities />
+        </div>
       </div>
-      <Price />
-      <Rating />
-      <Category />
-      <Activities />
     </div>
   );
 }
