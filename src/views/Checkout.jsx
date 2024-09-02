9cb354644dedf18cd7deba66f98043304097ad3e
Ahmedtell <hamada13678@gmail.com>
2024-09-02 17:03:26 +0300

    edit responsive

--- src/views/Checkout.jsx
+++ src/views/Checkout.jsx
@@ -7,20 +7,15 @@ import ImpInformation from "../components/molecules/ImpInformation";
 
 const CheckOut = () => {
   return (
-    <div className="px-4 py-6 lg:px-8">
-      <div className="booking-page flex flex-col lg:flex-row gap-6 lg:gap-8 my-6">
-        {/* Left Column */}
+    <div>
+      <div className="booking-page flex flex-col lg:flex-row gap-6 lg:gap-8 my-6 px-4 lg:px-0 ">
         <div className="left-column w-full lg:flex-1 space-y-6">
-          <h1 className="text-lg sm:text-xl font-semibold text-center lg:text-left">
-            Secure your reservation
-          </h1>
+          <h1 className="text-lg sm:text-xl font-semibold text-center lg:text-left">Secure your reservation</h1>
           <UserInfo />
           <PaymentOptions />
           <ImpInformation />
         </div>
-        
-        {/* Right Column */}
-        <div className="right-column w-full lg:w-1/3 space-y-6 mt-6 lg:mt-0">
+        <div className="right-column w-full lg:w-1/3 space-y-6">
           <Checking />
           <PriceDetails />
         </div>
